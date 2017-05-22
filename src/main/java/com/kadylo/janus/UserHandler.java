947e160189f8e7cfb7d1702f7d582399757e104c
HolyKadylo <pivas47@gmail.com>
2017-05-22 23:32:18 +0300

    ready to deploy

---
+++ src/main/java/com/kadylo/janus/UserHandler.java
@@ -0,0 +1,49 @@
+package com.kadylo.janus;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*; 
+import java.util.*;
+
+public class UserHandler extends HttpServlet{
+	private static volatile String instruction = null;
+	private static volatile String Answer = null;
+	
+	public String getAnswer(){
+		
+		return Answer;
+	}
+	
+	public setInstruction(String instruction){
+		this.instruction = instruction;
+	}
+	
+	
+	public static void main (String[] args){
+		System.out.println("UserHandler started");...
+	}
+	
+	// gets are sent from PC
+	// returns stored instruction
+	// ..........................
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
+		System.out.print("Doing UserHandler GET");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		if (instruction == null){
+			instruction = "echo Hello";
+		}
+		out.println(instruction);
+		System.out.println(", sent instruction " + instruction);
+	}
+	
+	// gets answers from HTTP POSTs
+	// . .       .      .      .
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
+		System.out.print("Doing UserHandler POST");
+		Answer = request.getParameter("i");
+		System.out.println(", got Answer " + Answer);
+	}
+}
