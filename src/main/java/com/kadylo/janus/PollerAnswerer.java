042a24eaad1cf6d13677b2b09aeb35c3b4790bf0
HolyKadylo <pivas47@gmail.com>
2017-05-27 00:53:54 +0300

    fixing logs

--- src/main/java/com/kadylo/janus/PollerAnswerer.java
+++ src/main/java/com/kadylo/janus/PollerAnswerer.java
@@ -20,24 +20,24 @@ public class PollerAnswerer extends HttpServlet{
 	// ..........................
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
-		System.out.print("-->Doing PollerAnswerer GET");
+		System.out.println("-->Doing PollerAnswerer GET");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
 		String instruction = Shuttle.access().getRequest();
 		
 		out.println(instruction);
-		System.out.println(", sent instruction " + instruction);
+		System.out.println("-->Sent instruction " + instruction);
 	}
 	
 	// gets answers from HTTP POSTs
 	// . .       .      .      .
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
-		System.out.print("-->Doing PollerAnswerer POST");
+		System.out.println("-->Doing PollerAnswerer POST");
 		
 		String answer = request.getParameter("payload");
-		System.out.println(", got Answer " + answer);
+		System.out.println("-->Got Answer " + answer);
 		
 		Shuttle.access().setResponce(answer);
 		Shuttle.access().getUserGate().interrupt();
--- src/main/java/com/kadylo/janus/UserHandler.java
+++ src/main/java/com/kadylo/janus/UserHandler.java
@@ -20,7 +20,7 @@ public class UserHandler extends HttpServlet{
 	// ..........................
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException  {
-		System.out.print("-->Doing UserHandler GET");
+		System.out.println("-->Doing UserHandler GET");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
@@ -38,6 +38,6 @@ public class UserHandler extends HttpServlet{
 		resp = Shuttle.access().getResponce();
 		
 		out.println(resp);
-		System.out.println(", sent instruction " + resp);
+		System.out.println("-->Sent instruction " + resp);
 	}
 }
